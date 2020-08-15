0e258533f8fb25b333d0c63495d981bbbc3b4413
arimintzu <albertus.ari310198@gmail.com>
2020-08-15 13:44:04 +0700

    TEst Mergre

--- Assets/Test Camera.cs
+++ Assets/Test Camera.cs
@@ -7,7 +7,8 @@ public class TestCamera : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("I want to test something one");
+        Debug.Log("I want to test something one");
     }
 
     // Update is called once per frame
